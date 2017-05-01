6d5fa3c368c28542eaf4d4a5479fc36735f7aa16
Adam Houston <phazyy9@gmail.com>
2017-05-01 15:48:08 +0100

    Updating chefspec to generate converge report

--- provision/adam-vagrant/spec/spec_helper.rb
+++ provision/adam-vagrant/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+at_exit { ChefSpec::Coverage.report! }
+
 RSpec.configure do |config|
   config.platform = 'centos'
   config.version = '7.2.1511'
