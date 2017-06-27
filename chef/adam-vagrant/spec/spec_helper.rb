fbb3103963038a31ceb9b10a66be0ffe616e4e6e
Adam Houston <phazyy9@gmail.com>
2017-06-27 18:55:07 +0100

    Updating RSpec config

--- chef/adam-vagrant/spec/spec_helper.rb
+++ chef/adam-vagrant/spec/spec_helper.rb
@@ -4,6 +4,10 @@ require 'chefspec/berkshelf'
 at_exit { ChefSpec::Coverage.report! }
 
 RSpec.configure do |config|
+  config.color = true               # Use color in STDOUT
+  config.formatter = :documentation # Use the specified formatter
+  config.log_level = :error         # Avoid deprecation notice SPAM
+
   config.platform = 'centos'
   config.version = '7.2.1511'
 end
