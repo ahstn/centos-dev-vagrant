56f0a4b977d1468f8324e0a48ccad921110f3ec9
Adam Houston <phazyy9@gmail.com>
2019-02-21 00:54:47 +0000

    Seperating ruby logic into lib/

---
+++ lib/vagrant.rb
@@ -0,0 +1,63 @@
+require 'yaml'
+
+# Cross-plaform audio support (misconfiguration can cause provisioning to fail).
+def configure_virtualbox_audio()
+  audio = nil
+  audiocontroller = 'hda'
+
+  if Vagrant::Util::Platform.windows?
+    audio  = 'dsound'
+  elsif Vagrant::Util::Platform.platform =~ /darwin/
+    audio  = 'coreaudio'
+  end
+
+  return audio, audiocontroller
+end
+
+# Check for Orcacle license acceptance, otherwise abort.
+def java_license_check(config)
+  agreement = 'I accept the "Oracle Binary Code License Agreement under the terms at http://www.oracle.com/technetwork/java/javase/terms/license/index.html'
+  if (!config['ansible']['skip_tags'].include? 'java') && config['java']['license_declaration'] != agreement
+      raise_message 'Aborting... to continue you must accept the Oracle License Agreement'
+    end
+end
+
+# Abort and print custom error message.
+def raise_message(msg)
+  raise Vagrant::Errors::VagrantError.new, msg
+end
+
+# Check plugins are installed, if not attempt to install them.
+def ensure_plugins(plugins)
+  logger = Vagrant::UI::Colored.new
+  installed = false
+
+  plugins.each do |plugin|
+    plugin_name = plugin['name']
+    manager = Vagrant::Plugin::Manager.instance
+
+    next if manager.installed_plugins.key?(plugin_name)
+
+    logger.warn("Installing plugin #{plugin_name}")
+
+    manager.install_plugin(
+      plugin_name,
+      sources: plugin.fetch('source', %w[https://rubygems.org/ https://gems.hashicorp.com/]),
+      version: plugin['version']
+    )
+
+    installed = true
+  end
+
+  return unless installed
+
+  logger.warn('`vagrant up` must be re-run now that plugins are installed')
+  exit
+end
+
+# Return a default post_up_message.
+def get_default_post_up_message(vconfig)
+  'Your Drupal VM Vagrant box is ready to use!'\
+    "\n* You can run `vagrant ssh` to access a shell inside the VM"
+    "\n* To safely stop the VM, run `vagrant halt`"
+end
